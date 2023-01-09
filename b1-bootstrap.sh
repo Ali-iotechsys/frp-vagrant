bb9e97ccb1e0df900f21779434b105c9ed0d113e
Ali Almohammad <ali@iotechsys.com>
2023-01-09 11:43:42 +0000

    Install docker

--- b1-bootstrap.sh
+++ b1-bootstrap.sh
@@ -2,6 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Install docker-ce
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker vagrant
+
 # Install FRP
 curl -LO https://github.com/fatedier/frp/releases/download/v0.46.0/frp_0.46.0_linux_amd64.tar.gz
 tar -xf frp_0.46.0_linux_amd64.tar.gz
--- b2-bootstrap.sh
+++ b2-bootstrap.sh
@@ -2,11 +2,17 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Install docker-ce
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker vagrant
+
+
 # Install FRP
 curl -LO https://github.com/fatedier/frp/releases/download/v0.46.0/frp_0.46.0_linux_amd64.tar.gz
 tar -xf frp_0.46.0_linux_amd64.tar.gz
 
-# Configuare (B2 as Private Server
+# Configuare (B2) as Private Server
 sed -i -e 's/server_addr = 127.0.0.1/server_addr = 192.168.33.10/g' /home/vagrant/frp_0.46.0_linux_amd64/frpc.ini
 sed -i -e 's/remote_port = 6000/remote_port = 6002/g' /home/vagrant/frp_0.46.0_linux_amd64/frpc.ini
 
